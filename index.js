672bc64d2b17430b18184b3e10953e46a9073cec
priyanka807 <priyankacoder11@gmail.com>
2025-01-21 18:20:42 +0530

    update server to index and add h1 tag

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(express.json());
 //routers
 app.use("/api", userRouter);
 
+//welcome to home page
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to the Backend Server!</h1>');
+});
 //app listen port
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
